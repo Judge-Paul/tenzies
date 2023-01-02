87b7a30aeb5774a89bc28703a5e40eed3b0bfaa8
Judge-Paul Ogebe <110723341+Judge-Paul@users.noreply.github.com>
2023-01-03 00:42:34 +0100

    feat: display number of rolls made and high score

--- src/App.jsx
+++ src/App.jsx
@@ -5,13 +5,16 @@ import Die from "./Die"
 export default function App() {
     const [dice, setDice] = React.useState(allNewDice())
     const [tenzies, setTenzies] = React.useState(false)
-    
+    const [tries, setTries] = React.useState(0)
+    const [highScore, setHighScore] = React.useState(100)
+
     React.useEffect(() => {
         const firstValue = dice[0].value
         const allHeld = dice.every(die => die.held)
         const allSameNumber = dice.every(die => die.value === firstValue)
         if(allHeld && allSameNumber) {
             setTenzies(true)
+            setHighScore(getHighScore())
         }
     }, [dice])
     
@@ -39,10 +42,17 @@ export default function App() {
                     die : 
                     { value: randomDieValue(), held: false, id: i + 1 }
             ))
+            setTries(prevTries => prevTries + 1)
         } else {
             setDice(allNewDice())
             setTenzies(false)
+            setTries(0)
         }
+    }
+    function getHighScore() {
+      let scoreToBeSet = highScore
+      tries <= highScore ? scoreToBeSet = tries : scoreToBeSet = highScore
+      return scoreToBeSet
     }
 
     function holdDice(id) {
@@ -56,12 +66,13 @@ export default function App() {
     const diceElements = dice.map((die) => (
         <Die key={die.id} {...die} hold={() => holdDice(die.id)} />
     ))
-
     return (
         <main>
             {tenzies && <Confetti />}
             <h1>Tenzies</h1>
-            <p>Roll until all dice are the same. Click each die to freeze it at its current value between rolls.</p>
+            {!tenzies && <p>Roll until all dice are the same. Click each die to freeze it at its current value between rolls.</p>}
+            {tenzies && <p>It took you {tries} rolls to complete the game do you want to go again.</p>}
+            {tenzies && <p>High Score: {highScore} rolls</p>}
             <div className="die-container">{diceElements}</div>
             <button className="roll-dice" onClick={rollUnheldDice}>
                 {tenzies ? "Reset Game" : "Roll"}
